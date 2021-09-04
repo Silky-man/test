40064d47f0afc6ac573c5df73346b4bb20ff39d5
Silky-man <35758431+Silky-man@users.noreply.github.com>
2021-09-04 16:18:13 +0800

    master

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
+#include<iostream>
+#include<list>
+#include<vector>
+#include<unordered_map>
+#include<set>
 
 using namespace std;
 
 int main()
 {
-    cout<<1<<endl;
+    list<int> tmplist;
+    vector<int> tmpvector;
+    unordered_map<int,int> tmpmap;
+    set<int> tmpset;
+
+    tmplist.resize(50);
+    tmpvector.resize(50);
+
+    cout<<tmplist.size()<<endl;
+    cout<<tmpvector.capacity()<<endl;
+    cout<<tmpmap.max_bucket_count()<<endl;
+    cout<<tmpset.max_size()<<endl;
+
     return 0;
 }
