c9d09808dc9dc0aeff3ca30c6e4b2d7e140c804b
Silky-man <35758431+Silky-man@users.noreply.github.com>
2021-09-19 09:52:52 +0800

    master

--- hello.cpp
+++ hello.cpp
@@ -1,25 +1,32 @@
 #include<iostream>
+
 #include<list>
 #include<vector>
 #include<unordered_map>
 #include<set>
+#include<deque>
+#include<forward_list>
+#include<queue>
+#include<stack>
+#include<map>
 
 using namespace std;
 
 int main()
 {
+    
     list<int> tmplist;
     vector<int> tmpvector;
+    deque<int> tmpdeque;
     unordered_map<int,int> tmpmap;
     set<int> tmpset;
-
-    tmplist.resize(50);
-    tmpvector.resize(50);
-
-    cout<<tmplist.size()<<endl;
-    cout<<tmpvector.capacity()<<endl;
-    cout<<tmpmap.max_bucket_count()<<endl;
-    cout<<tmpset.max_size()<<endl;
-
+    forward_list<int> tmpforlist;
+    queue<int> tmpque;
+    priority_queue<int> tmpprique;
+    stack<int> tmpstack;
+    map<string,int> tmpmap;
+    multimap<string,int> tmpmtmap;
+    multiset<int> tmpmtset;
+    
     return 0;
 }
